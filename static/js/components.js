e06f7d1ee8c6d71ab3c2509c3843e2eef611d069
bdy <bdy@bdyui-Macmini.local>
2021-11-16 17:12:51 +0900

    components

--- static/js/components.js
+++ static/js/components.js
@@ -2,17 +2,13 @@ const STATIC_PATH = 'static'
 
 Vue.component('main-view', {
     template: `
-        <div class="main-view-container">
+        <div class="cnt-c main-view">
             <img src="${STATIC_PATH}/img/congress.png">
-            <div class="logo">
-                <p>
-                    내 얼굴로 확인해보는
-                </p>
-                <p>
-                    운명의 <span style="color: #c39335;">정당</span> 찾기!
-                </p>
-            </div>
-            <button class="start-btn" v-on:click='startApp()'>
+            <p class="cnt-c slogan">
+                <a>내 얼굴로 확인해보는</a>
+                <a>운명의 <span class="pnt">정당</span> 찾기!</a>
+            </p>
+            <button class="btn" v-on:click='startApp()'>
                 시작하기
             </button>
         </div>
@@ -34,22 +30,20 @@ const partyColor = {
     7: '#661b85'
 }
 
-var shareUID = null;
-
 Vue.component('predict-view', {
     template: `
-        <div class="predict-view-container">
-            <section class="wait-view">
-                <div class="party-btn">
-                    <!-- 1 더불어민주당 --> <div style="border: 1px solid #004EA2" :style="party_num==1 ? {background: '${partyColor[1]}'} : {background: '#a52a2a00'}"></div>
-                    <!-- 2 국민의힘 --> <div style="border: 1px solid #E61E2B" :style="party_num==2 ? {background: '${partyColor[2]}'} : {background: '#a52a2a00'}"></div>
-                    <!-- 3 정의당--> <div style="border: 1px solid #FFCC00" :style="party_num==3 ? {background: '${partyColor[3]}'} : {background: '#a52a2a00'}"></div>
-                    <!-- 4 국민의당 --> <div style="border: 1px solid #EA5504" :style="party_num==4 ? {background: '${partyColor[4]}'} : {background: '#a52a2a00'}"></div>
-                    <!-- 5 열린민주당 --> <div style="border: 1px solid #003E9B" :style="party_num==5 ? {background: '${partyColor[5]}'} : {background: '#a52a2a00'}"></div>
-                    <!-- 6 기본소득당 --> <div style="border: 1px solid #82C8B4" :style="party_num==6 ? {background: '${partyColor[6]}'} : {background: '#a52a2a00'}"></div>
-                    <!-- 7 시대전한 --> <div style="border: 1px solid #661b85" :style="party_num==7 ? {background: '${partyColor[7]}'} : {background: '#a52a2a00'}"></div>
+        <div class="prdict-view">
+            <section>
+                <div class="cnt-r rot-container">
+                    <!-- 1 더불어민주당 --> <div class="rot-box" data-party="1" :style="party_num==1 ? {background: '${partyColor[1]}'} : {background: '#a52a2a00'}"></div>
+                    <!-- 2 국민의힘 --> <div class="rot-box" data-party="2" :style="party_num==2 ? {background: '${partyColor[2]}'} : {background: '#a52a2a00'}"></div>
+                    <!-- 3 정의당--> <div class="rot-box" data-party="3" :style="party_num==3 ? {background: '${partyColor[3]}'} : {background: '#a52a2a00'}"></div>
+                    <!-- 4 국민의당 --> <div class="rot-box" data-party="4" :style="party_num==4 ? {background: '${partyColor[4]}'} : {background: '#a52a2a00'}"></div>
+                    <!-- 5 열린민주당 --> <div class="rot-box" data-party="5" :style="party_num==5 ? {background: '${partyColor[5]}'} : {background: '#a52a2a00'}"></div>
+                    <!-- 6 기본소득당 --> <div class="rot-box" data-party="6" :style="party_num==6 ? {background: '${partyColor[6]}'} : {background: '#a52a2a00'}"></div>
+                    <!-- 7 시대전한 --> <div class="rot-box" data-party="7" :style="party_num==7 ? {background: '${partyColor[7]}'} : {background: '#a52a2a00'}"></div>
                 </div>
-                <div class="party-img">
+                <div class="cnt-r party-container">
                     <!-- 1 더불어민주당 --> <img v-show="party_num==1 && !predicted" class="party-face" src="${STATIC_PATH}/img/party-1-face.jpeg">
                     <!-- 1 더불어민주당 --> <img v-show="party_num==1" class="party-logo" src="${STATIC_PATH}/img/party-1-logo.png">
                     <!-- 2 국민의힘 --> <img v-show="party_num==2 && !predicted" class="party-face" src="${STATIC_PATH}/img/party-2-face.jpeg">
@@ -68,21 +62,23 @@ Vue.component('predict-view', {
             </section>
             <section class="upload-container">
                 <label for="upload-input">
-                    <div>
-                        <p><img id="upload-img" src="${STATIC_PATH}/img/upload.svg"></p>
-                        <p id="upload-text">사진 업로드</p>
+                    <div class="photo-box to-upload">
+                        <p class="cnt-c"><img id="upload-img" src="${STATIC_PATH}/img/upload.svg"></p>
+                        <p id="upload-text" class="cnt-c">사진 올리기</p>
                     </div>
                 </label>
+                <p class="ntc-photo">사진은 저장되지 않습니다.</p>
                 <input type="file" id="upload-input" name="upload-input" multiple accept="image/*" v-show=false v-on:change="loadPictureAndPredict">
             </section>
             <section class="result-container" style="display: none;">
-                <div>
-                    <button v-on:click="reload"><img src="${STATIC_PATH}/img/reload.svg"></button>
+                <div class="cnt-r photo-box uploaded">
+                    <button class="reload-btn" v-on:click="reload"><img src="${STATIC_PATH}/img/reload.svg"></button>
                     <img id="uploaded">
                 </div>
+                <p class="ntc-photo">사진은 저장되지 않습니다.</p>
                 <p id="predict-prop"></p>
                 <img src="${STATIC_PATH}/img/gold_badge.svg"/>
-                <button class="share-btn">공유하기</button>
+                <button class="btn">공유하기</button>
             </section>
         </div>
     `,
@@ -99,7 +95,8 @@ Vue.component('predict-view', {
             var img = document.getElementById('uploaded');
             img.src = url;
             
-            let mo = await getModel();
+            // let mo = await getModel();
+            let mo = model;
             let prediction = await mo.predict(img);
             
             // 가장 높은 정확도를 가진 정당 찾기.!
@@ -192,20 +189,5 @@ Vue.component('predict-view', {
             }) 
 
         }
-        // async share() {
-
-        //     // if (!shareUID) return;
-
-        //     const shareData = {
-        //         title: 'AI 얼굴 인식 정당 추천',
-        //         text: '내 얼굴로 확인해보는 운명의 정당 찾기!',
-        //         url: 'https://analysis-photo.com/',
-        //     }
-        //     try{
-        //         await navigator.share(shareData);
-        //     }catch(err){
-        //         console.log(err);
-        //     }
-        // }
     },
 });
